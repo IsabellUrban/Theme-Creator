f173583ce59179b6afcb876e47d8dd52c6872e53
Isabell Urban <mail@isabellurban.com>
2024-06-13 15:26:03 +0200

    add delete function

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,20 @@ import ColorForm from "./Components/ColorForm/ColorForm";
 import "./App.css";
 
 function App() {
-  const [colors, setColor] = useState(initialColors);
+  const [colors, setColors] = useState(initialColors);
 
   function handleAddColor(newColor) {
     console.log("Adding new color:", newColor);
-    setColor([{ id: uid(), ...newColor }, ...colors]);
+    setColors([{ id: uid(), ...newColor }, ...colors]);
+  }
+
+  function handleDeleteColor(id) {
+    console.log("Delete color:", id);
+    const colorsToKeep = colors.filter((color) => {
+      return color.id !== id;
+    });
+    console.log("Remaining colors:", colorsToKeep);
+    setColors(colorsToKeep);
   }
 
   return (
@@ -19,7 +28,13 @@ function App() {
       <ColorForm onAddColor={handleAddColor} />
       <br />
       {colors.map((color) => {
-        return <Color key={color.id} color={color} />;
+        return (
+          <Color
+            key={color.id}
+            color={color}
+            onDeleteColor={handleDeleteColor}
+          />
+        );
       })}
     </>
   );
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,15 @@
 import "./Color.css";
+import Button from "../Button/Button.jsx";
+import { useState } from "react";
 
-export default function Color({ color }) {
+export default function Color({ color, onDeleteColor }) {
+  const [isDelete, setIsDelete] = useState(false);
+
+  function handleToggleButton() {
+    isDelete ? setIsDelete(false) : setIsDelete(true);
+    // setIsDelete((prevState) => !prevState);
+  }
+
   return (
     <div
       className="color-card"
@@ -12,6 +21,27 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      {!isDelete ? (
+        <Button type="button" onClick={handleToggleButton}>
+          Delete
+        </Button>
+      ) : (
+        <>
+          <p className="color-card-highlight">Do you really want to delete?</p>
+          <Button type="button" onClick={handleToggleButton}>
+            Cancel
+          </Button>
+          <Button
+            type="button"
+            onClick={() => {
+              onDeleteColor(color.id);
+              setIsDelete(false);
+            }}
+          >
+            Delete
+          </Button>
+        </>
+      )}
     </div>
   );
 }
