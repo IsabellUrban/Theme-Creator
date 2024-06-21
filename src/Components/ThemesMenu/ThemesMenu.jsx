c56a74dfed60ef9b95db19af7e2f8f523d6369f7
Isabell Urban <mail@isabellurban.com>
2024-06-21 09:54:17 +0200

    add themes menue css

--- src/Components/ThemesMenu/ThemesMenu.jsx
+++ src/Components/ThemesMenu/ThemesMenu.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "../Button/Button";
+import "./ThemesMenu.css";
 
 export function ThemesMenu({ currentTheme, onChange, themes, onAddTheme }) {
   const [newThemeName, setNewThemeName] = useState("");
@@ -9,34 +10,44 @@ export function ThemesMenu({ currentTheme, onChange, themes, onAddTheme }) {
   };
 
   return (
-    <>
-      <label htmlFor="themes-menu">Choose a Theme: </label>
-      <select
-        name="themes-menu"
-        id="themes-menu"
-        value={currentTheme.id}
-        onChange={onChange}
-      >
-        {themes.map((theme) => (
-          <option key={theme.id} value={theme.id}>
-            {theme.name}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        value={newThemeName}
-        placeholder="Add new Theme Name"
-        onChange={handleThemeNameChange}
-      ></input>
-      <Button
-        type="button"
-        text="ADD"
-        onClick={() => {
-          onAddTheme(newThemeName);
-          setNewThemeName("");
-        }}
-      ></Button>
-    </>
+    <div className="themes-menu-box">
+      <div className="themes-menu-box">
+        <label htmlFor="themes-menu" style={{ marginRight: "10px" }}>
+          Choose a Theme:{" "}
+        </label>
+        <select
+          name="themes-menu"
+          id="themes-menu"
+          value={currentTheme.id}
+          onChange={onChange}
+        >
+          {themes.map((theme) => (
+            <option key={theme.id} value={theme.id}>
+              {theme.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="themes-menu-box-buttons">
+        <input
+          type="text"
+          value={newThemeName}
+          placeholder="Add new Theme Name"
+          onChange={handleThemeNameChange}
+        ></input>
+
+        <Button
+          type="button"
+          text="ADD"
+          onClick={() => {
+            onAddTheme(newThemeName);
+            setNewThemeName("");
+          }}
+        ></Button>
+        <Button type="button" text="EDIT" onClick={() => {}}></Button>
+
+        <Button type="button" text="DELETE" onClick={() => {}}></Button>
+      </div>
+    </div>
   );
 }
